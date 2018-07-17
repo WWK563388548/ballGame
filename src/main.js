67d7e99f19a212c1c32851d8054aef7d962c7ba0
wwk <ioh@geek.co.jp>
2018-07-17 18:37:43 +0900

    update and corrects some errors

--- src/main.js
+++ src/main.js
@@ -10,118 +10,111 @@ var player2Score = 0;
 
 var paddle1Y = 250;
 var paddle2Y = 250;
-// This value is a constant
-var PADDLE_HEIGHT = 100;
-var PADDLE_THICKNESS = 10;
+const PADDLE_THICKNESS = 10;
+const PADDLE_HEIGHT = 100;
 
-function calculateMousePosition(event) {
-
+function calculateMousePos(evt) {
     var rect = canvas.getBoundingClientRect();
-	var root = document.documentElement;
-	var mouseX = event.clientX - rect.left - root.scrollLeft;
-	var mouseY = event.clientY - rect.top - root.scrollTop;
-	return {
-		x:mouseX,
-		y:mouseY
-	};
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+        x:mouseX,
+        y:mouseY
+    };
 }
 
 window.onload = function() {
-
-    var framesPerSecond = 30;
-    canvas = document.getElementById("gameCanvas");
-
     // 创建 context 对象: context 对象是内建的 HTML5 对象，
     // 拥有多种绘制路径、矩形、圆形、字符以及添加图像的方法。
+    canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    setInterval(function() {
-        moveEverything();
-        drawEverything();
-    }, 1000/framesPerSecond);
 
-    canvas.addEventListener('mousemove', function(event) {
-		var mousePos = calculateMousePosition(event);
-		paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
-	});  
+    var framesPerSecond = 30;
+    setInterval(function() {
+            moveEverything();
+            drawEverything();   
+        }, 1000/framesPerSecond);
+
+    canvas.addEventListener('mousemove',
+        function(evt) {
+            var mousePos = calculateMousePos(evt);
+            paddle1Y = mousePos.y - (PADDLE_HEIGHT / 2);
+        });
 }
 
 function ballReset() {
-	ballSpeedX = -ballSpeedX;
-	ballX = canvas.width/2;
-	ballY = canvas.height/2;
+    ballSpeedX = -ballSpeedX;
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
 }
 
-function calculateMovement() {
+function computerMovement() {
     var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
     if(paddle2YCenter < ballY - 35) {
-		paddle2Y = paddle2Y + 6;
-	} else if(paddle2YCenter > ballY + 35) {
-		paddle2Y = paddle2Y - 6;
-	}
+        paddle2Y = paddle2Y + 6;
+    } else if(paddle2YCenter > ballY + 35) {
+        paddle2Y = paddle2Y - 6;
+    }
 }
 
 function moveEverything() {
-    
-    calculateMovement();
+    computerMovement();
 
     ballX = ballX + ballSpeedX;
-    ballX = ballY + ballSpeedY;
-
+    ballY = ballY + ballSpeedY;
+    
     if(ballX < 0) {
         if(ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
-			ballSpeedX = -ballSpeedX;
-		} else {
+            ballSpeedX = -ballSpeedX;
+        } else {
             ballReset();
-            player2Score++;	
-		}
+            player2Score++;
+        }
     }
-
     if(ballX > canvas.width) {
         if(ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT) {
-			ballSpeedX = -ballSpeedX;
-		} else {
-            ballReset();
-            player1Score++;	
-		}
+            ballSpeedX = -ballSpeedX;
+        } else {
+            ballReset();    
+            player1Score++;
+        }
     }
-
     if(ballY < 0) {
-		ballSpeedY = -ballSpeedY;
+        ballSpeedY = -ballSpeedY;
     }
-    
-	if(ballY > canvas.height) {
-		ballSpeedY = -ballSpeedY;
-	}
-    
+    if(ballY > canvas.height) {
+        ballSpeedY = -ballSpeedY;
+    }
 }
 
 // fillRect 方法拥有参数(225,210,200,200)
 // 在画布上绘制 200x200 的矩形，从坐标(225,210)开始
 function drawEverything() {
-
     // Background
     colorRect(0, 0, canvas.width, canvas.height, 'black');
+
     // Left player's paddle
     colorRect(0, paddle1Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
-    // Left player's paddle
+
+    // Right player's paddle
     colorRect(canvas.width - PADDLE_THICKNESS, paddle2Y, PADDLE_THICKNESS, PADDLE_HEIGHT, 'white');
+
     // Just a ball
     colorCircle(ballX, ballY, 10, 'white');
 
     canvasContext.fillText(player1Score, 100, 100);
-	canvasContext.fillText(player2Score, canvas.width - 100, 100);
+    canvasContext.fillText(player2Score, canvas.width - 100, 100);
 }
 
 function colorCircle(centerX, centerY, radius, drawColor) {
-
-	canvasContext.fillStyle = drawColor;
-	canvasContext.beginPath();
-	canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
-	canvasContext.fill();
+    canvasContext.fillStyle = drawColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+    canvasContext.fill();
 }
 
-function colorRect(leftX, topY, width, height, drawColor) {
-
-	canvasContext.fillStyle = drawColor;
-	canvasContext.fillRect(leftX, topY, width, height);
+function colorRect(leftX,topY, width,height, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
 }
